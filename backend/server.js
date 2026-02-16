c2f949f68e18440bca97afdda00b5bb1ce5b1f1f
Adedayo <atimileyin911@gmail.com>
2026-02-16 14:40:59 +0100

    ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,24 @@ import express from "express"
 import dotenv from "dotenv"
 import authRouters from "./router/auth.router.js"
 import messageRouter from "./router/messages.router.js"
+import path from "path"
 
 dotenv.config()
 
 const app = express()
-const PORT = process.env.PORT || 3000
+const __dirname = path.resolve()
+const PORT = process.env.PORT || 3001
+
+app.use(express.json())
 
 app.use("/api/auth",authRouters)
 app.use("/api/messages", messageRouter)
+if (process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
 
-app.listen(PORT,()=>console.log("app is listening on port 3000"))
+    app.get("*", (_,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+    })
+}
+
+app.listen(PORT,()=>console.log(`app is listening on port ${PORT}`))
